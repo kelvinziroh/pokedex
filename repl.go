9486cad44a1a0f64b93d51b9d836d4a2db2e296c
Kelvin Ziro <kelvin.ziroh@gmail.com>
2026-07-14 15:21:37 +0300

    Add command registry for pokedex cli

--- repl.go
+++ repl.go
@@ -24,7 +24,39 @@ func startRepl() {
 			continue
 		}
 		command := words[0]
-		fmt.Printf("Your command was: %s\n", command)
+
+		cmd, exists := getCommands()[command]
+		if exists {
+			err := cmd.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command: %s", command)
+			continue
+		}
 	}
 
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
 }
